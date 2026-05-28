41075e1dc2a13ee3a2e444b4aced5dffc62f4e12
Arcelyth <arcelyth@tutamail.com>
2026-05-29 01:57:04 +0800

    refactor simd.c; add more Vec4's functions

--- src/ffi/simd.c
+++ src/ffi/simd.c
@@ -16,75 +16,80 @@
     typedef struct { float f[4]; } vec_f32;
 #endif
 
-typedef struct {
+#if defined(_MSC_VER)
+    #define ALIGN_PRE(x) __declspec(align(x))
+    #define ALIGN_POST(x)
+#elif defined(__GNUC__) || defined(__clang__)
+    #define ALIGN_PRE(x)
+    #define ALIGN_POST(x) __attribute__((aligned(x)))
+#else
+    #define ALIGN_PRE(x) _Alignas(x)
+    #define ALIGN_POST(x)
+#endif
+
+typedef ALIGN_PRE(16) union {
     vec_f32 v;
-} Vec4f32;
+    float f[4];
+    struct {
+        float x, y, z, w;
+    };
+} ALIGN_POST(16) Vec4f32;
 
-Vec4f32 vec4f_new(float x, float y, float z, float w) {
-    Vec4f32 r;
+typedef ALIGN_PRE(16) union { vec_i32 v; int32_t i[4]; } ALIGN_POST(16) Vec4i32;
+typedef ALIGN_PRE(16) union { vec_u32 v; uint32_t u[4]; } ALIGN_POST(16) Vec4u32;
+typedef ALIGN_PRE(16) union { vec_u8 v; uint8_t b[16]; } ALIGN_POST(16) Vec4u8;
+
+#define VEC_RING_SIZE 512
+static Vec4f32 vec_ring[VEC_RING_SIZE] __attribute__((aligned(16)));
+static int vec_ring_idx = 0;
+
+inline static Vec4f32* alloc_ring_slot(void) {
+    Vec4f32* ptr = &vec_ring[vec_ring_idx];
+    vec_ring_idx = (vec_ring_idx + 1) & (VEC_RING_SIZE - 1);
+    return ptr;
+}
+
+Vec4f32* vec4f_new(float x, float y, float z, float w) {
+    Vec4f32* r = alloc_ring_slot();
 #if defined(__ARM_NEON) || defined(__aarch64__)
-    r.v = (float32x4_t){x, y, z, w};
+    r->v = (float32x4_t){x, y, z, w};
 #elif defined(__SSE__) || defined(__x86_64__)
-    r.v = _mm_set_ps(w, z, y, x);
+    r->v = _mm_set_ps(w, z, y, x);
 #else
-    r.v.f[0] = x; r.v.f[1] = y; r.v.f[2] = z; r.v.f[3] = w;
+    r->f[0] = x; r->f[1] = y; r->f[2] = z; r->f[3] = w;
 #endif
     return r;
 }
 
-Vec4f32 vec4f_add(Vec4f32 a, Vec4f32 b) {
-    Vec4f32 r;
+Vec4f32* vec4f_add(const Vec4f32* a, const Vec4f32* b) {
+    Vec4f32* r = alloc_ring_slot();
 #if defined(__ARM_NEON) || defined(__aarch64__)
-    r.v = vaddq_f32(a.v, b.v);
+    r->v = vaddq_f32(a->v, b->v);
 #elif defined(__SSE__) || defined(__x86_64__)
-    r.v = _mm_add_ps(a.v, b.v);
+    r->v = _mm_add_ps(a->v, b->v);
 #else
-    r.v.f[0] = a.v.f[0] + b.v.f[0];
-    r.v.f[1] = a.v.f[1] + b.v.f[1];
-    r.v.f[2] = a.v.f[2] + b.v.f[2];
-    r.v.f[3] = a.v.f[3] + b.v.f[3];
+    r->f[0] = a->f[0] + b->f[0];
+    r->f[1] = a->f[1] + b->f[1];
+    r->f[2] = a->f[2] + b->f[2];
+    r->f[3] = a->f[3] + b->f[3];
 #endif
     return r;
 }
 
-float vec4f_get_x(Vec4f32 a) {
-#if defined(__ARM_NEON) || defined(__aarch64__)
-    return vgetq_lane_f32(a.v, 0);
-#elif defined(__SSE__) || defined(__x86_64__)
-    return _mm_cvtss_f32(a.v);
-#else
-    return a.v.f[0];
-#endif
+float vec4f_get_x(const Vec4f32* a) {
+    return a->f[0];
 }
 
-float vec4f_get_y(Vec4f32 a) {
-#if defined(__ARM_NEON) || defined(__aarch64__)
-    return vgetq_lane_f32(a.v, 1);
-#elif defined(__SSE__) || defined(__x86_64__)
-    return _mm_cvtss_f32(_mm_shuffle_ps(a.v, a.v, _MM_SHUFFLE(1, 1, 1, 1)));
-#else
-    return a.v.f[1];
-#endif
+float vec4f_get_y(const Vec4f32* a) {
+    return a->f[1];
 }
 
-float vec4f_get_z(Vec4f32 a) {
-#if defined(__ARM_NEON) || defined(__aarch64__)
-    return vgetq_lane_f32(a.v, 2);
-#elif defined(__SSE__) || defined(__x86_64__)
-    return _mm_cvtss_f32(_mm_shuffle_ps(a.v, a.v, _MM_SHUFFLE(2, 2, 2, 2)));
-#else
-    return a.v.f[2];
-#endif
+float vec4f_get_z(const Vec4f32* a) {
+    return a->f[2];
 }
 
-float vec4f_get_w(Vec4f32 a) {
-#if defined(__ARM_NEON) || defined(__aarch64__)
-    return vgetq_lane_f32(a.v, 3);
-#elif defined(__SSE__) || defined(__x86_64__)
-    return _mm_cvtss_f32(_mm_shuffle_ps(a.v, a.v, _MM_SHUFFLE(3, 3, 3, 3)));
-#else
-    return a.v.f[3];
-#endif
+float vec4f_get_w(const Vec4f32* a) {
+    return a->f[3];
 }
 
 void vec4f_add_bulk(float* out, const float* a, const float* b, int count) {
@@ -114,6 +119,8 @@ int get_arch_info(void) {
 #elif defined(__SSE__) || defined(__x86_64__)
     return 2; // sse
 #else
-    return 0; // none 
+    return 0; // none
 #endif
 }
+
+
